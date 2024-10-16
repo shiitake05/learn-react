ba1f099ad3ad6e45e9439e54cfc19515bdb8fd51
shiitake05 <pegtar377512@gmail.com>
2024-10-16 22:56:09 +0900

    initial commit

---
+++ 01-quickstart/src/App.js
@@ -0,0 +1,197 @@
+// // 画面の更新にはuseStateを使う
+import { useState } from 'react';
+
+
+// // コンポーネントの作成
+// function MyButton() {
+//   return (
+//     <button>
+//       I'm a button
+//     </button>
+//   );
+// }
+
+// export default function MyApp() {
+//   return (
+//     <>
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//       {/* <AboutPage /> */}
+//     </div>
+//     </>
+//   );
+// }
+
+// // JSXでマークアップ
+// function AboutPage() {
+//   return (
+//     <>
+//       <h1>About</h1>
+//       <p>Hello there.<br />How do you do?</p>
+//     </>
+//   );
+// }
+
+
+// const user = {
+//   name: 'Hedy Lamarr',
+//   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+//   imageSize: 90,
+// };
+
+// // 文字列の連結
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt={'Photo of ' + user.name}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+
+// リストのレンダー
+// const products = [
+//   { title: 'Cabbage', isFruit: false, id: 1 },
+//   { title: 'Garlic', isFruit: false, id: 2 },
+//   { title: 'Apple', isFruit: true, id: 3 },
+// ];
+
+// // map()関数を使ってリストを作成
+// export default function ShoppingList() {
+//   const listItems = products.map(product =>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'magenta' : 'darkgreen'
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   );
+
+//   return (
+//     <ul>{listItems}</ul>
+//   );
+// }
+
+
+// イベントに応答する
+// function MyButton() {
+//   function handleClick() {
+//     alert('You clicked me!');
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   );
+// }
+
+
+// useStateは以下のように使う
+//  const [count, setCount] = useState(0);
+
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+
+// 複数の場所でレンダーした場合、それぞれが独自のstateを持つ
+// import { useState } from 'react';
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Counters that update separately</h1>
+//       <MyButton />
+//       <MyButton />
+//     </div>
+//   );
+// }
+
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+
+// このようにすることでボタンをクリックしてもMyAppのcountが更新されて両方のカウントが更新される
+// export default function MyApp() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Counters that update together</h1>
+//       <MyButton count={count} onClick={handleClick} />
+//       <MyButton count={count} onClick={handleClick} />
+//     </div>
+//   );
+// }
+
+// function MyButton({ count, onClick }) {
+//   return (
+//     <button onClick={onClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+
+
+// state のリフトアップによってstate をコンポーネント間で共有
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
